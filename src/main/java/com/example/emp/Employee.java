56f992e161fe777a11e3ba94a4c1036a53cea142
pranali096 <pranalipawar096@gmail.com>
2023-05-05 19:26:19 +0530

    Add Employee in employee entities

--- src/main/java/com/example/emp/Employee.java
+++ src/main/java/com/example/emp/Employee.java
@@ -1,5 +1,59 @@
 package com.example.emp;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="employee_tb")
 public class Employee {
+@Id
+@GeneratedValue(strategy = GenerationType.AUTO)
+@Column(name="empId")
+private Long id;
+
+@Column(name="empName")
+private String name;
+
+@Column(name="empCity")
+private String city;
+
+public Long getId() {
+	return id;
+}
+
+public void setId(Long id) {
+	this.id = id;
+}
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+public String getCity() {
+	return city;
+}
+
+public void setCity(String city) {
+	this.city = city;
+}
+
+public Employee() {
+	super();
+	// TODO Auto-generated constructor stub
+}
+
+@Override
+public String toString() {
+	return "Employee [id=" + id + ", name=" + name + ", city=" + city + "]";
+}
+
 
 }
